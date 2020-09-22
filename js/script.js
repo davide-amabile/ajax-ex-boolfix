a830bb9ad36f6f4a64b435e5018ccd3f3f7609bd
davide-amabile <davideamabile81@gmail.com>
2020-09-22 15:11:30 +0200

    inizio struttura

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  // creare evento click sul bottone
+  $(document).on("click", "#btn", function(){
+    var titolo = ($("#search").val();
+  });
+  // fine evento
+});
+// fine jquery
